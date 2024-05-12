bec43ef0d9a68a28903ed43d501d169d9ec36458
SergFediaev <sergfediaev@gmail.com>
2024-05-12 13:08:43 +0300

    3.1 - "Обработка ошибок: Задача"

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { setStatus } from '../../app/app-reducer.ts'
+import axios from 'axios'
 
 export const fetchDecksTC = () => (dispatch: Dispatch) => {
   dispatch(setStatus('loading'))
@@ -25,7 +26,16 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
 }
 
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
-    dispatch(updateDeckAC(res.data))
-  })
+  try {
+    // throw Error('Native error thrown!')
+    const response = await decksAPI.updateDeck(params)
+    dispatch(updateDeckAC(response.data))
+  } catch (error) {
+    let errorMessage: string
+
+    if (axios.isAxiosError(error)) errorMessage = error.response ? `Axios server response error: ${error.response.data.errorMessages[0].message}` : `Axios client error: ${error.message}`
+    else errorMessage = `Native error: ${(error as { message: string }).message}`
+
+    console.log(errorMessage)
+  }
 }
